6500f688fba8bf346c8cda2120b5797440848ad8
devid <devid@velorra.dev>
2026-06-21 14:15:13 +0530

    Initial Project Set Up

---
+++ src/test/java/me/smmukesh/ecommerceproject/EcommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package me.smmukesh.ecommerceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
