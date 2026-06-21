4e04744927544b0a50156a6ea6c897c50171f2c2
devid <devid@velorra.dev>
2026-06-21 14:54:53 +0530

    AddCategory endpoint testing

--- src/main/java/me/smmukesh/ecommerceproject/controller/CategoryController.java
+++ src/main/java/me/smmukesh/ecommerceproject/controller/CategoryController.java
@@ -1,9 +1,7 @@
 package me.smmukesh.ecommerceproject.controller;
 
 import me.smmukesh.ecommerceproject.model.Category;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
