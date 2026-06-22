829dae6941ac2505381c1eda4fa91e34228ece93
devid <devid@velorra.dev>
2026-06-22 14:51:14 +0530

    Updated the logic for Category ID temporarily.

--- src/main/java/me/smmukesh/ecommerceproject/service/CategoryService.java
+++ src/main/java/me/smmukesh/ecommerceproject/service/CategoryService.java
@@ -1,8 +1,6 @@
 package me.smmukesh.ecommerceproject.service;
 
 import me.smmukesh.ecommerceproject.model.Category;
-import me.smmukesh.ecommerceproject.repository.CategoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -10,16 +8,18 @@ import java.util.List;
 
 @Service
 public class CategoryService {
-    private CategoryRepository categoryRepository;
-
-    @Autowired
-    public CategoryService(CategoryRepository categoryRepository){
-        this.categoryRepository = categoryRepository;
-    }
+//    private CategoryRepository categoryRepository;
+//
+//    @Autowired
+//    public CategoryService(CategoryRepository categoryRepository){
+//        this.categoryRepository = categoryRepository;
+//    }
 
     List<Category> categories = new ArrayList<>();
+    private Long Id = 1L;
 
     public String createCategory(Category category){
+        category.setCategoryId(Id++);
         categories.add(category);
         return "Category Added successfully!";
     }
