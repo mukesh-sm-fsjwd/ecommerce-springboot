532bc35897b6596f40560fe12f2ee4aaa5188e21
devid <devid@velorra.dev>
2026-06-24 09:45:10 +0530

    Added Global Exception Handler to handle exceptions caused in all Rest Controllers.

---
+++ src/main/java/me/smmukesh/ecommerceproject/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package me.smmukesh.ecommerceproject.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> myMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        Map<String,String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(err ->{
+            String fieldName = ((FieldError)err).getField();
+            String reason = err.getDefaultMessage();
+            errors.put(fieldName,reason);
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errors);
+    }
+
+}
