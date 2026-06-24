1818598eb087753549b9814aab52f6cd02980b5c
devid <devid@velorra.dev>
2026-06-24 17:01:00 +0530

    Moved Jakarta Validation Constraints to RequestClass

--- src/main/java/me/smmukesh/ecommerceproject/model/Category.java
+++ src/main/java/me/smmukesh/ecommerceproject/model/Category.java
@@ -1,8 +1,6 @@
 package me.smmukesh.ecommerceproject.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,7 +16,5 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
 
-    @NotBlank(message = "Category Name should not be blank.")
-    @Size(min = 5,message = "Category Name atleast should contain 5 characters")
     private String categoryName;
 }
