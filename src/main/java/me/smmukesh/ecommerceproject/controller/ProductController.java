2fd08b2605393e8f5e66ab21110705046b77ab36
devid <smmukesh.ai369@gmail.com>
2026-06-27 18:24:59 +0530

    Added Product Controller and Repository

---
+++ src/main/java/me/smmukesh/ecommerceproject/controller/ProductController.java
@@ -0,0 +1,27 @@
+package me.smmukesh.ecommerceproject.controller;
+
+import me.smmukesh.ecommerceproject.dto.request.ProductRequest;
+import me.smmukesh.ecommerceproject.model.Product;
+import me.smmukesh.ecommerceproject.service.CategoryService;
+import me.smmukesh.ecommerceproject.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api")
+public class ProductController {
+    private ProductService productService;
+    private CategoryService categoryService;
+
+    public ProductController(ProductService productService, CategoryService categoryService){
+        this.productService = productService;
+        this.categoryService = categoryService;
+    }
+
+    @PostMapping("admin/categories/{categoryId}/product")
+    public ResponseEntity<ProductRequest> addProduct(@RequestBody Product product,@PathVariable Long categoryId){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(productService.addProduct(product,categoryId));
+    }
+}
---
+++ src/main/java/me/smmukesh/ecommerceproject/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package me.smmukesh.ecommerceproject.repository;
+
+import me.smmukesh.ecommerceproject.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+}
