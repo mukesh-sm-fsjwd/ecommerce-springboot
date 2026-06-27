9bb7d7a10c4cb96490b9ae37d54510889f16add0
devid <smmukesh.ai369@gmail.com>
2026-06-27 18:24:19 +0530

    Added Product Entity.

---
+++ src/main/java/me/smmukesh/ecommerceproject/model/Product.java
@@ -0,0 +1,27 @@
+package me.smmukesh.ecommerceproject.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+    private String productName;
+    private String image;
+    private String description;
+    private Integer quantity;
+    private Double price;
+    private Double discount;
+    private Double specialPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
