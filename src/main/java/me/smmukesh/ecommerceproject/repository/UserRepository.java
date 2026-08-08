cd00f1c903208ce3df000fda0a5346b33d552dc2
devid <smmukesh.ai369@gmail.com>
2026-08-08 11:27:06 +0530

    Implemented Custom UserRepository to load all the details of the user from the database table.

---
+++ src/main/java/me/smmukesh/ecommerceproject/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package me.smmukesh.ecommerceproject.repository;
+
+import me.smmukesh.ecommerceproject.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByUserName(String email);
+}
