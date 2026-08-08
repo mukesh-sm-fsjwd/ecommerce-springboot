a38f60a6b268712180e2a875b627e6d39d1481b8
devid <smmukesh.ai369@gmail.com>
2026-08-08 11:26:33 +0530

    Implemented CustomUserDetailsService class for loading the UserDetails with the username.

---
+++ src/main/java/me/smmukesh/ecommerceproject/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package me.smmukesh.ecommerceproject.security.service;
+
+import me.smmukesh.ecommerceproject.model.User;
+import me.smmukesh.ecommerceproject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User with the name "+username+" not found !"));
+        return UserDetailsImpl.build(user);
+    }
+}
