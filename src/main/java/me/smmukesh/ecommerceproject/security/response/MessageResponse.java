7d8e17ce84d6a0e420ccdb9ad987c2aefd9734e0
devid <smmukesh.ai369@gmail.com>
2026-08-15 17:28:57 +0530

    Created and Implemented Custom MessageResponse and used in Signup endpoint.

---
+++ src/main/java/me/smmukesh/ecommerceproject/security/response/MessageResponse.java
@@ -0,0 +1,14 @@
+package me.smmukesh.ecommerceproject.security.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+}
