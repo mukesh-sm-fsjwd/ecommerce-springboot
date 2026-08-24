fc5884986f4e6a6a056a5705d8c5df73c60ac30e
devid <smmukesh.ai369@gmail.com>
2026-08-24 19:54:04 +0530

    Created And Implemented CartRepository and CartItemRepository.

--- src/main/java/me/smmukesh/ecommerceproject/repository/CartRepository.java
+++ src/main/java/me/smmukesh/ecommerceproject/repository/CartRepository.java
@@ -2,8 +2,14 @@ package me.smmukesh.ecommerceproject.repository;
 
 import me.smmukesh.ecommerceproject.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart,Long> {
+
+    @Query("SELECT c FROM Cart c WHERE c.user.email = ?1")
+    Optional<Cart> findCartByEmail(String email);
 }
