378e8f6c303d0a9392059c33e686bcbf7ee6d7a2
devid <smmukesh.ai369@gmail.com>
2026-08-25 12:54:03 +0530

    Created getCartByEmailAndCartId method in CartRepository with Custom query.

--- src/main/java/me/smmukesh/ecommerceproject/repository/CartRepository.java
+++ src/main/java/me/smmukesh/ecommerceproject/repository/CartRepository.java
@@ -4,7 +4,6 @@ import me.smmukesh.ecommerceproject.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
@@ -12,4 +11,7 @@ public interface CartRepository extends JpaRepository<Cart,Long> {
 
     @Query("SELECT c FROM Cart c WHERE c.user.email = ?1")
     Optional<Cart> findCartByEmail(String email);
+
+    @Query("SELECT c FROM Cart c WHERE c.user.email = ?1 AND c.cartId = ?2")
+    Optional<Cart> getCartByEmailAndCartId(String emailId, Long cartId);
 }
