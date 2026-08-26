77d3de38843bb99ae2e4a440f0f7e634ffe23520
devid <smmukesh.ai369@gmail.com>
2026-08-26 17:49:45 +0530

    Defined a new Custom repository method to find the carts by the product id using custom query.

--- src/main/java/me/smmukesh/ecommerceproject/repository/CartRepository.java
+++ src/main/java/me/smmukesh/ecommerceproject/repository/CartRepository.java
@@ -4,6 +4,8 @@ import me.smmukesh.ecommerceproject.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +16,7 @@ public interface CartRepository extends JpaRepository<Cart,Long> {
 
     @Query("SELECT c FROM Cart c WHERE c.user.email = ?1 AND c.cartId = ?2")
     Optional<Cart> getCartByEmailAndCartId(String emailId, Long cartId);
+
+    @Query("SELECT c FROM Cart c JOIN FETCH c.cartItems ci JOIN FETCH ci.product p WHERE p.productId = ?1")
+    List<Cart> findCartsByProductId(long productId);
 }
