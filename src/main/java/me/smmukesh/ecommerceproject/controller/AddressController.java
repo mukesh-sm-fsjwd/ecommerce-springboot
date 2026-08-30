bf506a3aff19afabea0fa2da40d8f49c6ca4a3ff
devid <smmukesh.ai369@gmail.com>
2026-08-30 12:51:33 +0530

    Created and Implemented Address Controller.

---
+++ src/main/java/me/smmukesh/ecommerceproject/controller/AddressController.java
@@ -0,0 +1,33 @@
+package me.smmukesh.ecommerceproject.controller;
+
+import jakarta.validation.Valid;
+import me.smmukesh.ecommerceproject.dto.request.AddressDTO;
+import me.smmukesh.ecommerceproject.model.User;
+import me.smmukesh.ecommerceproject.service.AddressService;
+import me.smmukesh.ecommerceproject.utils.AuthUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class AddressController {
+    private final AddressService addressService;
+    private final AuthUtils authUtils;
+
+    public AddressController(AddressService addressService,AuthUtils authUtils) {
+        this.addressService = addressService;
+        this.authUtils = authUtils;
+    }
+
+    @PostMapping("/addresses")
+    public ResponseEntity<AddressDTO> createAddress(@Valid @RequestBody AddressDTO addressDTO){
+        User user = authUtils.loggedInUser();
+        AddressDTO address = addressService.createAddress(addressDTO,user);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(address);
+    }
+}
