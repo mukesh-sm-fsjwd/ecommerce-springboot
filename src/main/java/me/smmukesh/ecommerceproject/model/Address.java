fc52b9c0b227dc857e966bccb095683358736deb
devid <smmukesh.ai369@gmail.com>
2026-08-30 12:08:09 +0530

    Removed Validation Annotations and used that in dto class for better architecture.

--- src/main/java/me/smmukesh/ecommerceproject/model/Address.java
+++ src/main/java/me/smmukesh/ecommerceproject/model/Address.java
@@ -1,8 +1,6 @@
 package me.smmukesh.ecommerceproject.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -19,28 +17,16 @@ public class Address {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer addressId;
 
-    @NotBlank
-    @Size(min = 5,message = "The Street name must be at least 5 characters in size.")
     private String street;
 
-    @NotBlank
-    @Size(min = 5,message = "The Building name must be at least 5 characters in size.")
     private String buildingName;
 
-    @NotBlank
-    @Size(min = 4,message = "The City name must be at least 4 characters in size.")
     private String city;
 
-    @NotBlank
-    @Size(min = 2,message = "The State name must be at least 2 characters in size.")
     private String state;
 
-    @NotBlank
-    @Size(min = 2,message = "The Country name must be at least 2 characters in size.")
     private String country;
 
-    @NotBlank
-    @Size(min = 6,message = "The pin code must be at least 6 characters in size.")
     private String pincode;
 
     @ManyToMany(mappedBy = "addresses")
