b459c40969b099289bc4eb835d39c0804e7d6a0f
devid <smmukesh.ai369@gmail.com>
2026-08-30 12:52:06 +0530

    Created and Implemented Address Repository.

---
+++ src/main/java/me/smmukesh/ecommerceproject/repository/AddressRepository.java
@@ -0,0 +1,9 @@
+package me.smmukesh.ecommerceproject.repository;
+
+import me.smmukesh.ecommerceproject.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address,Long> {
+}
