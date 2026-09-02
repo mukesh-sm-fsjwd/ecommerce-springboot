909f1c5f7a7290370143b84333d47e1399ebbe9b
devid <smmukesh.ai369@gmail.com>
2026-09-02 21:05:45 +0530

    Created OrderItemRepository.

---
+++ src/main/java/me/smmukesh/ecommerceproject/repository/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package me.smmukesh.ecommerceproject.repository;
+
+import me.smmukesh.ecommerceproject.model.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
+}
