71dfbc6d71e18c0eab37da404acade0474952dae
devid <smmukesh.ai369@gmail.com>
2026-09-02 22:14:35 +0530

    Implemented Order Products API Logic on service layer.

--- src/main/java/me/smmukesh/ecommerceproject/service/OrderService.java
+++ src/main/java/me/smmukesh/ecommerceproject/service/OrderService.java
@@ -45,5 +45,74 @@ public class OrderService {
         this.cartService = cartService;
     }
 
+    @Transactional
+    public OrderDTO orderProducts(String emailId, String paymentMethod, OrderRequestDTO orderRequestDTO) {
+        //! 1. Get User Cart.
+        Cart cart = cartRepository.findCartByEmail(emailId)
+                .orElseThrow(() -> new ResourceNotFoundException("Cart","Email",emailId));
+        Long addressId = orderRequestDTO.getAddressId();
+        Address address = addressRepository.findById(addressId)
+                .orElseThrow(() -> new ResourceNotFoundException("Address","Address Id",addressId));
 
+
+        //* 2. Create a new order with payment info.
+        Order order = new Order();
+        order.setEmail(emailId);
+        order.setOrderDate(LocalDate.now());
+        order.setTotalAmount(cart.getTotalPrice());
+        order.setOrderStatus(OrderStatus.PROCESSING);
+        order.setAddress(address);
+
+        String pgName = orderRequestDTO.getPgName();
+        String pgPaymentId = orderRequestDTO.getPgPaymentId();
+        String pgStatus = orderRequestDTO.getPgStatus();
+        String pgResponseMessage = orderRequestDTO.getPgResponseMessage();
+
+        Payment payment = new Payment(paymentMethod,pgPaymentId,pgStatus,pgResponseMessage,pgName);
+        payment.setOrder(order);
+        payment = paymentRepository.save(payment);
+        order.setPayment(payment);
+
+        Order savedOrder = orderRepository.save(order);
+
+        //? 3. Get items from the cart into the order items.
+        List<CartItem> cartItems = cart.getCartItems();
+        if (cartItems.isEmpty()){
+            throw new APIException("Cart is Empty.");
+        }
+
+        List<OrderItem> orderItems = new ArrayList<>();
+        for(CartItem cartItem : cartItems){
+            OrderItem orderItem = new OrderItem();
+            orderItem.setProduct(cartItem.getProduct());
+            orderItem.setQuantity(cartItem.getQuantity());
+            orderItem.setDiscount(cartItem.getDiscount());
+            orderItem.setOrderedProductPrice(cartItem.getProductPrice());
+            orderItem.setOrder(savedOrder);
+            orderItems.add(orderItem);
+        }
+
+        orderItems = orderItemRepository.saveAll(orderItems);
+
+        //! 4. Update Product Stock.
+        cart.getCartItems().forEach(item -> {
+            int quantity = item.getQuantity();
+            Product product = item.getProduct();
+            product.setQuantity(product.getQuantity() - quantity);
+            productRepository.save(product);
+
+            //* 5. Clear the cart.
+            cartService.deleteProductFromCart(cart.getCartId(), item.getProduct().getProductId());
+        });
+
+        //? 6. Send back the order summary.
+        OrderDTO orderDTO = modelMapper.map(savedOrder,OrderDTO.class);
+        orderItems.forEach(item ->
+                orderDTO.getOrderItems().add(
+                        modelMapper.map(item, OrderItemDTO.class)
+                )
+        );
+        orderDTO.setAddressId(addressId);
+        return orderDTO;
+    }
 }
